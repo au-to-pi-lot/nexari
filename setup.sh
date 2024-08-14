0c874324b50b9566768c6c31f155404458699300
Ink Brownell <inkbrownell@gmail.com>
2024-08-14 01:44:41 -0500

    fix: Remove apt-get from setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Update package list and install required packages
-sudo apt-get update
-sudo apt-get install -y python3-pip python3-venv
-
 # Create a virtual environment
 python3 -m venv venv
 source venv/bin/activate
