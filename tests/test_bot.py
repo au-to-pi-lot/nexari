abe47c321432f4346357547829c482cf9fb70198
Ink Brownell <inkbrownell@gmail.com>
2024-08-20 23:12:30 -0500

    feat: Add pytest

---
+++ tests/test_bot.py
@@ -0,0 +1,44 @@
+from src.bot import DiscordBot
+from src.const import DISCORD_MESSAGE_MAX_CHARS
+
+
+def test_break_messages_preserves_single_newlines():
+    original_text = "hello world\nhello universe\nhello multiverse"
+    expected_result = [original_text]
+    assert DiscordBot.break_messages(original_text) == expected_result
+
+
+def test_break_messages_breaks_on_double_newlines():
+    original_text = "hello world\n\nhello_universe\n\nhello multiverse"
+    expected_result = original_text.split("\n\n")
+    assert DiscordBot.break_messages(original_text) == expected_result
+
+
+def test_break_messages_breaks_long_text():
+    original_text = " ".join(["test"] * 10000)
+    results = DiscordBot.break_messages(original_text)
+    for result in results:
+        assert len(result) <= DISCORD_MESSAGE_MAX_CHARS
+
+
+def test_break_messages_avoids_breaking_short_code_blocks():
+    original_text = """\
+```
+import antigravity
+
+antigravity.engage()
+```
+"""
+    assert DiscordBot.break_messages(original_text) == original_text
+
+def test_break_messages_inserts_backticks_around_breaks_in_long_code_blocks():
+    newline = "\n"  # necessary for syntax reasons
+    original_text = f"""\
+```
+{newline.join(["test"] * 1000)}
+```
+"""
+    result = DiscordBot.break_messages(original_text)
+    for result in result:
+        assert result.startswith("```\n")
+        assert result.endswith("\n```")
