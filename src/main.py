cf0e30f937ed0b3f29329a864ee53144db8800fb
Ink Brownell <inkbrownell@gmail.com>
2024-08-25 17:03:12 -0500

    fix: Refactor main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-import asyncio
-
 from config import config
 from src.bot import DiscordBot
 
@@ -7,16 +5,9 @@ from src.bot import DiscordBot
 def main():
     """
     Main function to start the Discord bot.
-
-    This function creates a DiscordBot instance with the bot configuration,
-    starts it in the event loop, and runs the loop indefin
-itely.
     """
     bot = DiscordBot(config.bot)
-    loop = asyncio.get_event_loop()
-    loop.create_task(bot.start(bot.config.discord.bot_token))
-    loop.run_forever()
-
+    bot.start()
 
 if __name__ == "__main__":
     try:
