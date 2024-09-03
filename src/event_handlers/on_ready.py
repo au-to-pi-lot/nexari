f9b587f820b6a068fdf9d50eb3069db2ad223c69
Ink Brownell <inkbrownell@gmail.com>
2024-09-03 03:17:32 -0500

    fix: Fix on_ready

--- src/event_handlers/on_ready.py
+++ src/event_handlers/on_ready.py
@@ -1,24 +1,27 @@
-from discord import Client
-from svcs import Container
+import logging
 
-from src.bot import logger
-from src.services import registry
+from discord.ext.commands import Bot
+
+from src.proxies import GuildProxy
+from src.services import svc
+
+logger = logging.getLogger(__name__)
 
 
 async def on_ready():
     """
     Called when the bot is ready and connected to Discord.
     """
-    client = Container(registry=registry).get(Client)
-    logger.info(f"{client.user} has connected to Discord!")
+    bot = svc.get(Bot)
+    logger.info(f"{bot.user} has connected to Discord!")
 
-    for guild in client.guilds:
-        await client.ensure_guild_exists(guild)
+    for guild in bot.guilds:
+        await GuildProxy.get(guild.id)
 
     try:
-        guild = await client.fetch_guild(307011228293660683)
-        client.tree.copy_global_to(guild=guild)
-        synced = await client.tree.sync(guild=guild)
+        guild = await bot.fetch_guild(307011228293660683)
+        bot.tree.copy_global_to(guild=guild)
+        synced = await bot.tree.sync(guild=guild)
         logger.info(f"Synced {len(synced)} command(s)")
     except Exception as e:
         logger.exception(f"Error syncing command tree: {e}")
