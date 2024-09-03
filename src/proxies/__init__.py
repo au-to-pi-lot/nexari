699db90e49ffdcf49c6b7f26e3059fc3f076afe6
Ink Brownell <inkbrownell@gmail.com>
2024-09-03 03:11:49 -0500

    feat: Add LLMProxy to proxies module

--- src/proxies/__init__.py
+++ src/proxies/__init__.py
@@ -1,7 +1,8 @@
 from .channel import ChannelProxy
 from .guild import GuildProxy
 from .webhook import WebhookProxy
-from .message_formatter import MessageFormatter
+from .llm import LLMProxy
 
-__all__ = ["ChannelProxy", "GuildProxy", "WebhookProxy", "MessageFormatter"]
+
+__all__ = ["ChannelProxy", "GuildProxy", "WebhookProxy", "LLMProxy"]
 
