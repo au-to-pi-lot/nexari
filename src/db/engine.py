b37262efe27f3fce2fab76ffe0d47836b66fc652
Ink Brownell <inkbrownell@gmail.com>
2024-09-03 01:32:43 -0500

    chore: Remove engine.py

--- src/db/engine.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from src.config import config
-
-engine = create_async_engine(config.database_url)
-Session = async_sessionmaker(engine)
