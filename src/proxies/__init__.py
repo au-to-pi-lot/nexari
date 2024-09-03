13906d729f8da2a235850c20583e76414f9d5ee3
Ink Brownell <inkbrownell@gmail.com>
2024-09-03 02:06:14 -0500

    feat: add MessageFormatter import and include in __all__ list

--- src/proxies/__init__.py
+++ src/proxies/__init__.py
@@ -1,6 +1,7 @@
 from .channel import ChannelProxy
 from .guild import GuildProxy
 from .webhook import WebhookProxy
+from .message_formatter import MessageFormatter
 
-__all__ = ["ChannelProxy", "GuildProxy", "WebhookProxy"]
+__all__ = ["ChannelProxy", "GuildProxy", "WebhookProxy", "MessageFormatter"]
 
