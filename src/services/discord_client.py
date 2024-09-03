ca07819aa977fea6910749e588e304cf93b18f18
Ink Brownell <inkbrownell@gmail.com>
2024-09-03 01:32:32 -0500

    fix: Make single Discord client for entire app

--- src/services/discord_client.py
+++ src/services/discord_client.py
@@ -6,7 +6,8 @@ from src.services import registry
 intents: discord.Intents = discord.Intents.default()
 intents.message_content = True
 
+bot = Bot(command_prefix="!", intents=intents)
 registry.register_factory(
     svc_type=Bot,
-    factory=lambda: Bot(command_prefix="!", intents=intents)
+    factory=lambda: bot
 )
