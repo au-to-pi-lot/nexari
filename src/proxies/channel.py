0f8908034da79acccbea1210d851401987131aeb
Ink Brownell <inkbrownell@gmail.com>
2024-09-04 16:44:15 -0500

    feat: Return MessageProxy instead of discord.Message in ChannelProxy

--- src/proxies/channel.py
+++ src/proxies/channel.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union
+from typing import Optional, Union, List
 
 import discord
 from discord.ext.commands import Bot
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.db.models import Channel as DBChannel
 from src.services import svc
 from src.types.proxy import BaseProxy
+from src.proxies.message import MessageProxy
 
 
 class ChannelProxy(BaseProxy[discord.TextChannel, DBChannel]):
@@ -42,14 +43,17 @@ class ChannelProxy(BaseProxy[discord.TextChannel, DBChannel]):
     def guild(self) -> discord.Guild:
         return self._discord_obj.guild
 
-    async def send(self, content: str = None, **kwargs) -> discord.Message:
-        return await self._discord_obj.send(content, **kwargs)
+    async def send(self, content: str = None, **kwargs) -> MessageProxy:
+        discord_message = await self._discord_obj.send(content, **kwargs)
+        return await MessageProxy.get(discord_message.id)
 
-    async def fetch_message(self, message_id: int) -> discord.Message:
-        return await self._discord_obj.fetch_message(message_id)
+    async def fetch_message(self, message_id: int) -> MessageProxy:
+        discord_message = await self._discord_obj.fetch_message(message_id)
+        return await MessageProxy.get(discord_message.id)
 
-    async def purge(self, limit: int = 100, **kwargs) -> list[discord.Message]:
-        return await self._discord_obj.purge(limit=limit, **kwargs)
+    async def purge(self, limit: int = 100, **kwargs) -> List[MessageProxy]:
+        discord_messages = await self._discord_obj.purge(limit=limit, **kwargs)
+        return [await MessageProxy.get(message.id) for message in discord_messages]
 
     async def set_permissions(self, target: Union[discord.Role, discord.Member], **permissions):
         await self._discord_obj.set_permissions(target, **permissions)
@@ -60,5 +64,5 @@ class ChannelProxy(BaseProxy[discord.TextChannel, DBChannel]):
     async def webhooks(self) -> list[discord.Webhook]:
         return await self._discord_obj.webhooks()
 
-    async def history(self, **kwargs) -> list[discord.Message]:
-        return [message async for message in self._discord_obj.history(**kwargs)]
+    async def history(self, **kwargs) -> List[MessageProxy]:
+        return [await MessageProxy.get(message.id) async for message in self._discord_obj.history(**kwargs)]
