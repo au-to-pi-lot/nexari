c69924b34c543a2222dda09e8bbe9c0897123df4
Ink Brownell <inkbrownell@gmail.com>
2024-09-05 16:55:00 -0500

    fix: render_as_batch for sqlite

--- migrations/env.py
+++ migrations/env.py
@@ -44,6 +44,7 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        render_as_batch=url.startswith("sqlite"),
     )
 
     with context.begin_transaction():
@@ -62,9 +63,14 @@ def run_migrations_online() -> None:
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
+    url = config.get_main_option("sqlalchemy.url")
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            render_as_batch=url.startswith("sqlite"),
+        )
 
         with context.begin_transaction():
             context.run_migrations()
