e09df4e5c890755eb3a4658424bb8cda281bc240
AUTOPILOT <zuthayis@gmail.com>
2024-10-29 20:30:58 -0500

    fix: Remove unnecessary validator

--- src/config.py
+++ src/config.py
@@ -7,12 +7,6 @@ class Config(BaseSettings):
     client_id: str
     database_url: str
 
-    # noinspection PyNestedDecorators
-    @field_validator("client_id", mode="before")
-    @classmethod
-    def transform_client_id_to_str(cls, value) -> str:
-        return str(value)
-
     class Config:
         env_prefix = ""
         case_sensitive = False
