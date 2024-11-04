0c8b0da3ad26e4ddaf39a84755bfecb3bf0dbd7b
AUTOPILOT <zuthayis@gmail.com>
2024-11-03 22:45:41 -0600

    Revert "fix: remove extra @ from PostgreSQL connection URL"
    This reverts commit 87a7f917142ca5fb29b7028154a7be1318136c51.

--- terraform/main.tf
+++ terraform/main.tf
@@ -114,7 +114,7 @@ resource "google_secret_manager_secret" "database_url" {
 resource "google_secret_manager_secret_version" "database_url" {
   secret = google_secret_manager_secret.database_url.id
   secret_data = replace(
-    "postgresql+asyncpg://${google_sql_user.user.name}:${random_password.db_password.result}/${google_sql_database.database.name}?host=/cloudsql/${google_sql_database_instance.instance.connection_name}",
+    "postgresql+asyncpg://${google_sql_user.user.name}:${random_password.db_password.result}@/${google_sql_database.database.name}?host=/cloudsql/${google_sql_database_instance.instance.connection_name}",
     "%",
     "%%"
   )
