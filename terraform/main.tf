700955adad3a107b96e27e23147ddf554b529345
AUTOPILOT <zuthayis@gmail.com>
2024-11-07 02:57:28 -0600

    fix: use IP address instead of name for VPC peering range

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,7 +51,7 @@ resource "google_compute_global_address" "private_ip_address" {
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.vpc_network.id
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.address]
 }
 
 # Enable required APIs
